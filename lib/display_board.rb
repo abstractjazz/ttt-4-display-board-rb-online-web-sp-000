2ee0da4226130aed65ba08f126db69f700a3724a
Evan Burton <evan@heyevan.com>
2020-03-15 19:46:11 +0000

    Automatically backed up by Learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -1,9 +1,9 @@
 board = [" ", " ", " ", " ", " ", " ", " ", " ", " "]
 
 def display_board(board)
-  puts " #{board[0]}  | #{board[1]}    | #{board[3]}   "
+  puts "#{board[0]}  | #{board[1]}    | #{board[3]}   "
   puts "-----------"
-  puts " #{board[4]}  | #{board[5]}    | #{board[6]}   "
+  puts "#{board[4]}  | #{board[5]}    | #{board[6]}   "
   puts "-----------"
-  puts "#{board[9]}   | #{board[10]}   | #{board[11]}  "
+  puts "#{board[9]}  | #{board[10]}   | #{board[11]}  "
 end
